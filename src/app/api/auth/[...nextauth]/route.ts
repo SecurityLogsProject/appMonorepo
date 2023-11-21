2e387a3bef511331c0df990098ef4a8b1007eb0e
Chekiro <chekiro@wp.pl>
2023-11-21 09:46:02 +0100

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,6 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-
-const prisma = new PrismaClient()
-
-export const authOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-    ],
-    secred: process.env.NEXTAUTH_SECRET,
-}
+import { authOptions } from '@/utils/authOptions'
+import NextAuth from 'next-auth/next'
 
 const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST, authOptions }
