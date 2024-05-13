c674ab0ef9ca1c2d86ab99342ef1f3312c3918c3
Kamil Turek <kamil_turek@protonmail.ch>
2024-05-13 17:01:14 +0200

    FEAT: add logs

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -2,35 +2,29 @@ import { PrismaClient } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 const prisma = new PrismaClient()
 
-const postHandler = async (
-    req: Request,
-)  => {
-    console.log("TEST")
+const postHandler = async (req: Request) => {
     if (req.method === 'POST') {
-        const {machineId,content} = await req.json();
-        if (!machineId || !content) throw new Error('Params missing!')
-        console.log(`Adding logs for machine with id ${machineId}`);
+        const { id, content } = await req.json()
+        if (!id || !content) throw new Error('Params missing!')
+        console.log(`Adding logs for machine with id ${id}`)
         const machine = await prisma.machine.update({
             where: {
-                id: machineId,
+                id: id,
             },
             data: {
                 logs: {
                     create: {
                         content: content,
-                    }
-                }
-            }
+                    },
+                },
+            },
         })
-
-
-        console.log(machine)
-        return  Response.json({message: 'ok'})
+        return Response.json({ message: 'ok' })
     } else {
         return new Response('There was an error while saving logs', {
-            status: 400
-        });
+            status: 400,
+        })
     }
 }
 
-export {postHandler as POST}
+export { postHandler as POST }
